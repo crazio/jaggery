98ca88ff8fb5c13350c4b90872f8c2ae5bb7ac74
crazio <uladzislau.malmyha@leverx.com>
2019-12-19 16:21:40 +0300

    Db connection issue resolved for Java module

--- backend/blockchain/server.js
+++ backend/blockchain/server.js
@@ -1,22 +1,8 @@
-const http = require("http");
-const express = require("express");
-const dotenv = require('dotenv');
-dotenv.config();
-const Web3Singleton = require("./utils/Web3Singleton");
+const app = require("express")();
+//const cds = require("@sap/cds");
+//
+//cds.connect.to("db");
+//cds.serve("all").in(app);
 
-const port = process.env.PORT || 3000;
-
-const server = http.createServer();
-const app = express();
-
-const web3 = Web3Singleton.getInstance();
-
-app.use(express.json());
-
-app.get("/accounts", async (req, res) => {
-    const accounts = await web3.eth.getAccounts();
-    res.status(200).send(accounts);
-});
-
-server.on("request", app);
-server.listen(port, () => console.log("Server listening on port %d", port));
+ const PORT = process.env.PORT || 4004;
+ app.listen(PORT);
